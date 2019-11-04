f0a4fb7aee571b0e52ab4bd7b90833e3d790fa06
marcus <marcus@acelera.cloud>
2019-11-04 16:08:32 -0300

    [ADD] #silent - Adicionei suport a silent, que é uma forma de interromper o debug via runtime do código.

--- src/main.js
+++ src/main.js
@@ -1,39 +1,43 @@
 const { SuperConsole } = require('af-super-console');
 
-let start = null;
-const EventMonitor = function (emitter, type) {
-  var emit = emitter && emitter.emit;
-  if (typeof emit !== 'function') return;
+const EventMonitor = {
+  _start: null,
+  silent: false,
+  debug(emitter, type) {
+    var emit = emitter && emitter.emit;
+    if (typeof emit !== 'function') return;
 
-  emitter.emit = function (event, ...args) {
-    var end = Date.now();
-    var diff = start === null ? 0 : end - start;
-    start = end;
+    emitter.emit = function (event, ...args) {
+      if (EventMonitor.silent) return undefined;
+      var end = Date.now();
+      var diff = _start === null ? 0 : end - _start;
+      _start = end;
 
-    if (args.length > 0) {
-      if (Array.isArray(args)) args = args.join(', ');
-      if (typeof args === 'object') args = JSON.stringify(args);
-      SuperConsole.groupLog({
-        groupColor: 'yellow',
-        reasonColor: 'grey',
-        messageColor: 'cyan',
-        group: type || emitter.constructor.name,
-        message: event,
-        reason: `Duração: ${diff}ms - Argumentos: (${args})`,
-      });
-    } else {
-      SuperConsole.groupLog({
-        groupColor: 'yellow',
-        reasonColor: 'magenta',
-        messageColor: 'cyan',
-        group: type || emitter.constructor.name,
-        message: event,
-        reason: `Duração: ${diff}ms`,
-      })
-    }
+      if (args.length > 0) {
+        if (Array.isArray(args)) args = args.join(', ');
+        if (typeof args === 'object') args = JSON.stringify(args);
+        SuperConsole.groupLog({
+          groupColor: 'yellow',
+          reasonColor: 'grey',
+          messageColor: 'cyan',
+          group: type || emitter.constructor.name,
+          message: event,
+          reason: `Duração: ${diff}ms - Argumentos: (${args})`,
+        });
+      } else {
+        SuperConsole.groupLog({
+          groupColor: 'yellow',
+          reasonColor: 'magenta',
+          messageColor: 'cyan',
+          group: type || emitter.constructor.name,
+          message: event,
+          reason: `Duração: ${diff}ms`,
+        })
+      }
 
-    return emit.apply(this, arguments);
-  };
+      return emit.apply(this, arguments);
+    };
+  }
 };
 
 module.exports = EventMonitor;
